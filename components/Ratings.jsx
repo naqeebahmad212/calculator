8a23b79ea8965e862436fc2af1569a9095a76367
naqeebahmad212 <naqeebahmad212@gmail.com>
2024-02-10 10:50:40 +0500

    sixth commit

--- components/Ratings.jsx
+++ components/Ratings.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { styled } from "@mui/material/styles";
 import Rating from "@mui/material/Rating";
 import FavoriteIcon from "@mui/icons-material/Favorite";
@@ -15,26 +16,32 @@ const StyledRating = styled(Rating)({
 });
 
 const Ratings = () => {
-  if (typeof window !== "undefined") {
-    const rating = JSON.parse(localStorage.getItem("ratings"));
-    return (
-      <>
-        <span className="p-3">
-          <StyledRating
-            name="customized-color"
-            defaultValue={Number(rating)}
-            getLabelText={(value) => `${value} Heart${value !== 1 ? "s" : ""}`}
-            precision={0.5}
-            icon={<FavoriteIcon fontSize="inherit" />}
-            emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
-            onChange={(e) =>
-              localStorage.setItem("ratings", JSON.stringify(e.target.value))
-            }
-          />
-        </span>
-      </>
-    );
-  }
+  const [rating, setRating] = useState(0);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const item = JSON.parse(localStorage.getItem("ratings"));
+      setRating(Number(item));
+    }
+  });
+
+  return (
+    <>
+      <span className="p-3">
+        <StyledRating
+          name="customized-color"
+          value={rating}
+          defaultValue={3}
+          getLabelText={(value) => `${value} Heart${value !== 1 ? "s" : ""}`}
+          precision={0.5}
+          icon={<FavoriteIcon fontSize="inherit" />}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+          onChange={(e) =>
+            localStorage.setItem("ratings", JSON.stringify(e.target.value))
+          }
+        />
+      </span>
+    </>
+  );
 };
 
 export default Ratings;
